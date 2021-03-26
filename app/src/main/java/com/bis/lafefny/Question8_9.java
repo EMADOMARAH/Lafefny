c5ab4f9c5028441a75124c6787bd216cd1309a02
Malak Hesham <malakh328@gmail.com>
2021-03-26 02:38:24 +0200

    First part in Questionnaire added

---
+++ app/src/main/java/com/bis/lafefny/Question8_9.java
@@ -0,0 +1,14 @@
+package com.bis.lafefny;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Question8_9 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question8_9);
+    }
+}
