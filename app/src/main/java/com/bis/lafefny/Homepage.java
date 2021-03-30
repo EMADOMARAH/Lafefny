c839d90e9f9420e633a883c120f2af4e7372d7bb
ahmedaminlafefny <80847422+ahmedaminlafefny@users.noreply.github.com>
2021-03-31 00:04:03 +0200

    Homepage.java

--- app/src/main/java/com/bis/lafefny/Homepage.java
+++ app/src/main/java/com/bis/lafefny/Homepage.java
@@ -1,21 +1,38 @@
 package com.bis.lafefny;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.bis.lafefny.R;
+import com.google.android.material.navigation.NavigationView;
 
-public class Homepage extends AppCompatActivity {
+public class Homepage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    //Buttons
     private Button button_ent;
     private Button button_event;
     private Button button_plan;
     private Button button_emg;
+
+    //Menu
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +40,61 @@ public class Homepage extends AppCompatActivity {
         setContentView(R.layout.activity_homepage);
         Toast.makeText(getBaseContext(),"Process Success..",Toast.LENGTH_LONG);
 
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                int id = menuItem.getItemId();
+
+                switch (id) {
+                    case R.id.nav_home:
+
+                        Intent aboutusintent = new Intent(getApplicationContext(), Cinema.class);
+                        aboutusintent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(aboutusintent);
+                        break;
+                    case R.id.nav_cycle:
+                        Toast.makeText(getApplicationContext(), "Training Programmes", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawers();
+
+                        break;
+
+
+//                        SharedPreferences preferences = getSharedPreferences("MyPREFERENCES", Context.MODE_PRIVATE);
+//                        if (preferences.contains("password")) {
+//                            SharedPreferences.Editor editor = preferences.edit();
+//                            editor.remove("password");
+//                            editor.commit();
+//                        }
+                        //finish(); // Call once you redirect to another activity.
+
+
+                }
+                return true;
+            }
+        });
+
+        //Menu
+
+        /*--------------------Hooks-----------------------------*/
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        /*--------------------Tool Bar-----------------------------*/
+        setSupportActionBar(toolbar);
+
+        /*--------------------Navigation Drawer Menu-----------------------------*/
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //Buttons
         button_ent = (Button) findViewById(R.id.btn_entertainment); //button amusement park
         button_ent.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,14 +119,9 @@ public class Homepage extends AppCompatActivity {
             }
         });
 
-        button_emg = (Button) findViewById(R.id.btn_emg); //button emergency
-        button_emg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openEmergency();
-            }
-        });
+
     }
+
     public void openEnt_Categories(){
         Intent intent = new Intent(this, Ent_Categories.class); //open entertainment categories
         startActivity(intent);
@@ -68,8 +135,18 @@ public class Homepage extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void openEmergency(){
-        Intent intent = new Intent(this, Emergency.class); //open emergency
-        startActivity(intent);
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+//Menu
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
     }
 }
