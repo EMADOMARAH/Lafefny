b92a0f763eebb8b680e238473649166260bee90c
Malak Hesham <malakh328@gmail.com>
2021-05-24 00:39:56 +0200

    About

--- app/src/main/java/com/bis/lafefny/About.java
+++ app/src/main/java/com/bis/lafefny/About.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 
 public class About extends AppCompatActivity {
     private Button button_home_About;
+    private Button button_pre_About;
+    private Button button_account_About;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +26,33 @@ public class About extends AppCompatActivity {
                 openHomepage();
             }
         });
+
+        button_pre_About = (Button) findViewById(R.id.btn_pre_hp); //button preferences
+        button_pre_About.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openpreferences();
+            }
+        });
+
+        button_account_About = (Button) findViewById(R.id.btn_user_hp); //button account
+        button_account_About.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAccount();
+            }
+        });
     }
 
 
     public void openHomepage() {
         startActivity(new Intent(getApplicationContext(), Homepage.class));  //open homepage
+    }
+
+    public void openpreferences() {
+        startActivity(new Intent(getApplicationContext(), preferences.class));  //open preferences
+    }
+    public void openAccount() {
+        startActivity(new Intent(getApplicationContext(), Account.class));  //open account
     }
 }
